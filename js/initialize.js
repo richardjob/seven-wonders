ebc6231365b48e91292a83c90f986f55bfaf7308
Richard Job <richardjob05@gmail.com>
2020-10-01 11:53:49 +0530

    added card contents in index

--- js/initialize.js
+++ js/initialize.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var sidenav = document.querySelectorAll(".sidenav");
-  var Isidenav = M.Sidenav.init(sidenav);
-
   var slider = document.querySelectorAll('.slider');
   var Islider = M.Slider.init(slider, );
 });
