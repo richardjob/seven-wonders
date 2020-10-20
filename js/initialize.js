2302143d33d33503749e3fc59c5ad29962059397
Richard Job <richardjob05@gmail.com>
2020-10-20 19:01:18 +0530

    removed unwanted JS

--- js/initialize.js
+++ js/initialize.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var slider = document.querySelectorAll('.slider');
   var Islider = M.Slider.init(slider);
-
-  var pushpin = document.querySelectorAll('.pushpin');
-  var Ipushpin = M.Pushpin.init(pushpin);
 });
